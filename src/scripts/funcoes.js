42d76cb40cc43afb3b9245b0eae54a43419b9b1c
Anderson Benicio <andyrodrigo2@gmail.com>
2023-12-15 18:18:44 -0300

    duelo criado

--- src/scripts/funcoes.js
+++ src/scripts/funcoes.js
@@ -1,61 +1,77 @@
-function desenharCartao(indice) {
-  //console.log(cartas[indice]);
-  // estado.cartaoExibido.imagem.src = cartas[indice].imagem;
-  estado.cartaoExibido.tipo.innerText = "Tipo: " + cartas[indice].tipo;
-  estado.cartaoExibido.nome.innerText = "Nome: " + cartas[indice].nome;
-  estado.cartaoExibido.poder.innerText = "Poder : " + cartas[indice].poder;
-  estado.cartaoExibido.efeito.innerText = "Efeito: " + cartas[indice].efeito;
+function moverCartaParaUltimaPosicao(id, deck) {
+  const indice = deck.findIndex((objeto) => objeto.id === id);
+
+  if (indice !== -1 && indice !== deck.length - 1) {
+    const carta = deck.splice(indice, 1)[0];
+    deck.push(carta);
+  }
+  //console.log("deck: ", deck);
 }
 
-function atribuirCarta(carta, indice) {
-  const id = estado.jogador.personagem.cartas[indice].id;
-  carta.setAttribute("data-id", id);
-  if (!estado.batalha.iniciado) {
-    carta.addEventListener("mouseover", () => {
-      //console.log(carta.getAttribute("data-id"));
-      desenharCartao(carta.getAttribute("data-id"));
-      estado.telas.telaJogar.style.display = "none";
-    });
-    carta.addEventListener("mousedown", () => {
-      //console.log(carta.getAttribute("data-id"));
-      const cartaAtual = carta.getAttribute("data-id");
-      desenharCartao(cartaAtual);
-      estado.telas.telaJogar.style.display = "flex";
-      estado.batalha.selecionadoJogador = cartaAtual;
-      estado.cartaoExibido.cartaSelecionada.innerText = cartas[cartaAtual].nome;
-      //console.log("cartaAtual: ", estado.batalha.selecionadoJogador);
-    });
+function testarFimDeJogo(energia, resultado) {
+  if (energia <= 0) {
+    estado.batalha.iniciado = false;
+    setTimeout(() => {
+      estado.vista.mensagemBatalha.textContent = `Você ${resultado} a duelo!`;
+      setTimeout(() => {
+        sair();
+      }, 3000);
+    }, 3000);
   }
 }
 
-function definirCartas(inicial) {
-  cartaDaRodada = [
-    estado.cartasEmCampo.carta1,
-    estado.cartasEmCampo.carta2,
-    estado.cartasEmCampo.carta3,
-    estado.cartasEmCampo.carta4,
-    estado.cartasEmCampo.carta5,
-  ];
-  if (inicial === 15) estado.batalha.posicaoDeck = 0;
-  for (let i = 0; i < 5; i++) {
-    let id = inicial + i;
-    if (id >= 15) id -= 15;
-    atribuirCarta(cartaDaRodada[i], id);
+function realizarEfeito(jogadorAtual, carta) {
+  const id = carta.id;
+  switch (id) {
+    case 13:
+      console.log("olho");
+      estado[jogadorAtual].energia += 500;
+      break;
+    default:
+      break;
   }
 }
 
-function mover() {
-  estado.batalha.posicaoDeck++;
-  definirCartas(estado.batalha.posicaoDeck);
-}
+function realizarResultado(resultado) {
+  if (resultado === "venceu") {
+    const poder = estado.jogador.cartaSelecionada.poder;
+    estado.computador.energia -= poder;
+    testarFimDeJogo(estado.computador.energia, resultado);
+    if (estado.batalha.iniciado) {
+      realizarEfeito("jogador", estado.jogador.cartaSelecionada);
+    }
+  } else if (resultado === "perdeu") {
+    const poder = estado.computador.cartaSelecionada.poder;
+    estado.jogador.energia -= poder;
+    testarFimDeJogo(estado.jogador.energia, resultado);
+    if (estado.batalha.iniciado) {
+      realizarEfeito("computador", estado.jogador.cartaSelecionada);
+    }
+  } else {
+    realizarEfeito("jogador", estado.jogador.cartaSelecionada);
+    realizarEfeito("computador", estado.jogador.cartaSelecionada);
+  }
 
-function iniciar() {
-  console.log("foi");
-  estado.telas.telaInicial.style.display = "none";
-  estado.telas.telaJogo.style.display = "flex";
+  if (estado.batalha.iniciado) {
+    estado.vista.energiaComputador.textContent = estado.computador.energia;
+    estado.vista.energiaJogador.textContent = estado.jogador.energia;
 
-  definirCartas(0);
-  estado.batalha.iniciado = true;
+    moverCartaParaUltimaPosicao(
+      estado.jogador.cartaSelecionada.id,
+      estado.jogador.deck
+    );
+
+    moverCartaParaUltimaPosicao(
+      estado.computador.cartaSelecionada.id,
+      estado.computador.deck
+    );
+
+    estado.batalha.turno++;
+
+    setTimeout(() => {
+      estado.telas.telaBatalha.style.display = "none";
+    }, 3000);
+  }
 }
 
 function verificarVitoria(tipo1, tipo2) {
@@ -79,19 +95,133 @@ function verificarVitoria(tipo1, tipo2) {
 }
 
 function analisarCombate() {
-  const indiceAleatorio = Math.floor(Math.random() * yugi.cartas.length);
-  cartaComputador = yugi.cartas[indiceAleatorio];
-  cartaJogador = yugi.cartas[estado.batalha.selecionadoJogador];
-  console.log("Carta do PC", cartaComputador);
-  console.log("Carta do Jogador", cartaJogador);
+  const indiceAleatorio = Math.floor(Math.random() * 5);
 
-  const resultado = verificarVitoria(cartaJogador.tipo, "Spock"); //cartaComputador.tipo);
-  console.log("Resultado", resultado);
+  cartaJogador = estado.jogador.cartaSelecionada;
+  cartaComputador = estado.computador.deck[indiceAleatorio];
+  estado.computador.cartaSelecionada = cartaComputador;
+
+  //console.log("Carta do PC", cartaComputador);
+  //console.log("Carta do Jogador", cartaJogador);
+
+  const resultado = verificarVitoria(cartaJogador.tipo, cartaComputador.tipo);
+
+  estado.vista.mensagemBatalha.textContent = `${resultado.mensagem}! Jogador ${resultado.resultado}! `;
+
+  realizarResultado(resultado.resultado);
+
+  //console.log("Resultado", resultado);
 }
 
 function jogar() {
-  console.log("jogou");
   estado.telas.telaJogar.style.display = "none";
   estado.telas.telaBatalha.style.display = "flex";
+  //console.log("carta em jogo: ", estado.jogador.cartaSelecionada);
   analisarCombate();
+}
+
+function desenharCartao(carta) {
+  // estado.cartaoExibido.imagem.src = cartas.imagem;
+  estado.cartaoExibido.tipo.innerText = "Tipo: " + carta.tipo;
+  estado.cartaoExibido.nome.innerText = "Nome: " + carta.nome;
+  estado.cartaoExibido.poder.innerText = "Poder : " + carta.poder;
+  estado.cartaoExibido.efeito.innerText = "Efeito: " + carta.efeito;
+}
+
+function buscarCarta(deck, id) {
+  return deck.find((carta) => carta.id === id);
+}
+
+function atribuirCarta(carta, id) {
+  carta.setAttribute("data-id", id);
+
+  if (!estado.batalha.iniciado) {
+    const dataId = Number(carta.getAttribute("data-id"));
+
+    carta.addEventListener("mouseover", () => {
+      desenharCartao(estado.jogador.deck[dataId]);
+      estado.telas.telaJogar.style.display = "none";
+    });
+    carta.addEventListener("mousedown", () => {
+      desenharCartao(estado.jogador.deck[dataId]);
+      estado.telas.telaJogar.style.display = "flex";
+      estado.jogador.cartaSelecionada = estado.jogador.deck[dataId];
+      estado.cartaoExibido.cartaSelecionada.innerText =
+        estado.jogador.deck[dataId].nome;
+    });
+  }
+}
+
+function definirCartas() {
+  const cartaDaRodada = [
+    estado.cartasEmCampo.carta1,
+    estado.cartasEmCampo.carta2,
+    estado.cartasEmCampo.carta3,
+    estado.cartasEmCampo.carta4,
+    estado.cartasEmCampo.carta5,
+  ];
+
+  for (let i = 0; i < 5; i++) {
+    atribuirCarta(cartaDaRodada[i], i);
+  }
+}
+
+function teste() {
+  console.log("teste");
+  const carta = buscarCarta(cartas, 0);
+}
+
+function mostrarProximo() {
+  const id = estado.jogador.deck[5].id;
+  const carta = buscarCarta(estado.jogador.personagem.cartas, id);
+  desenharCartao(carta);
+}
+
+function embaralharDeck(deck) {
+  const novoDeck = [...deck];
+  for (let i = novoDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [novoDeck[i], novoDeck[j]] = [novoDeck[j], novoDeck[i]];
+  }
+
+  return novoDeck;
+}
+
+function zerarPlacares() {
+  estado.jogador.energia = 2000;
+  estado.computador.energia = 2000;
+  estado.jogador.efeitos = [];
+  estado.computador.efeitos = [];
+  estado.batalha.turno = 1;
+  estado.jogador.cartaSelecionada = undefined;
+  estado.computador.cartaSelecionada = undefined;
+}
+
+function sair() {
+  estado.telas.telaInicial.style.display = "block";
+  estado.telas.telaJogo.style.display = "none";
+  estado.telas.telaBatalha.style.display = "none";
+  estado.telas.telaJogar.style.display = "none";
+  estado.batalha.iniciado = false;
+}
+
+function iniciar() {
+  zerarPlacares();
+
+  estado.vista.energiaComputador.textContent = estado.computador.energia;
+  estado.vista.energiaJogador.textContent = estado.jogador.energia;
+  desenharCartao(cartaVazia);
+
+  estado.telas.telaInicial.style.display = "none";
+  estado.telas.telaJogo.style.display = "flex";
+
+  estado.jogador.deck = embaralharDeck(estado.jogador.personagem.cartas);
+  estado.computador.deck = embaralharDeck(estado.computador.personagem.cartas);
+
+  //console.log("Deck Jogador: ", estado.jogador.deck);
+  //console.log("Deck Computador: ", estado.computador.deck);
+
+  definirCartas();
+
+  estado.batalha.iniciado = true;
 }
